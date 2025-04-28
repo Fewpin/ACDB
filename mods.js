f0407f0e9bd98cc8c9de31e6522fc4e68c668cb5
Fewpin <135955380+Fewpin@users.noreply.github.com>
2025-04-28 21:24:51 +0900

    Create mods.js

---
+++ mods.js
@@ -0,0 +1,83 @@
+// mods.js
+const mods = [
+  {
+    thumbnail: "https://www.shinacmod.com/wp-content/uploads/2024/01/Screenshot_slang_ferrari_f40_ks_red_bull_ring_29-0-124-14-55-3.jpg",
+    name: "Ferrari F40 Custom",
+    category: "車両",
+    author: "Opperslang",
+    download: "https://opperslang.gumroad.com/l/ferrarif40custom",
+    rating: 5,
+    memo: "精密アニメーション",
+    tags: ["street", "Italy"]
+  },
+  {
+    thumbnail: "https://c10.patreonusercontent.com/4/patreon-media/p/post/94779071/a392faef81554f1796c7f8b8f5c7bf10/eyJ3Ijo2MjB9/1.jpg?token-time=1747094400&token-hash=1UtNrSBUTc0nqFalq6itZwF1K-iwKCtXoZuaUoo_QFk%3D",
+    name: "Chevrolet Corvette C8 Z06 Convertible",
+    category: "車両",
+    author: "Binary Motors(Jerry Wei)",
+    download: "https://www.patreon.com/posts/release-corvette-94779071",
+    rating: 5,
+    memo: "decrypted",
+    tags: ["street", "USA"]
+  },
+  {
+    thumbnail: "https://public-files.gumroad.com/hszwf59ved8hc10xdjq8j9irjwkw",
+    name: "Porsche 911 991 Street Cup",
+    category: "車両",
+    author: "Igna More",
+    download: "https://igna15workshop.gumroad.com/l/izmuo",
+    rating: 5,
+    memo: "decrypted",
+    tags: ["street", "Germany"]
+  },
+  {
+    thumbnail: "https://public-files.gumroad.com/ty6pqmegnerqc2o1su9145rq6imc",
+    name: "Mazda RX-7 FD3S Street/Drift",
+    category: "車両",
+    author: "Igna More",
+    download: "https://igna15workshop.gumroad.com/l/FDRX7",
+    rating: 5,
+    memo: "decrypted",
+    tags: ["street", "日本"]
+  },
+  {
+    thumbnail: "https://public-files.gumroad.com/fekejlid5xlud4pvcbi6wtd992fy",
+    name: "BMW M3 E46 N-Spec",
+    category: "車両",
+    author: "Igna More",
+    download: "https://igna15workshop.gumroad.com/l/m3e46",
+    rating: 5,
+    memo: "encrypted",
+    tags: ["street", "Germany"]
+  },
+  {
+    thumbnail: "https://c10.patreonusercontent.com/4/patreon-media/p/post/122355066/f8a756165709494e82221f7682f06ea3/eyJ3Ijo2MjB9/1.png?token-time=1747094400&token-hash=R3tZd0mIXMPXQhj5EHHv66RoXcJ7pWNiPCi7qYRZT4w%3D",
+    name: "Toyota Supra MK4 TRD",
+    category: "車両",
+    author: "MNBA",
+    download: "https://www.patreon.com/posts/toyota-supra-mk4-122355066",
+    rating: 5,
+    memo: "encrypted",
+    tags: ["street", "日本"]
+  },
+  {
+    thumbnail: "https://ac.totsugeki.com/wp-content/custom/mods/tracks/3183/shuto_revival_project_beta/ui/tatsumi_pa/preview.png",
+    name: "Shutoko Revival Project",
+    category: "コース",
+    author: "SRP",
+    download: "https://shutokorevivalproject.com/",
+    rating: 5,
+    memo: "traffic in discord",
+    tags: ["street", "日本"]
+  },
+  {
+    thumbnail: "https://cdn.buymeacoffee.com/uploads/rewards/2024-08-18/1/195334_Yar_1.jpg@1200w_0e.jpg",
+    name: "Toyota GR Yaris RZ",
+    category: "車両",
+    author: "JohnnieJar",
+    download: "https://buymeacoffee.com/johnniejar/e/291420",
+    rating: 5,
+    memo: "v1.2 decrypted",
+    tags: ["street", "日本"]
+  }
+];
